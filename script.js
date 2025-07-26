e635f903176b7bb88b15bc2d69c451ff3c0545b3
Aaron <kirkffrimpong@gmail.com>
2025-07-26 18:10:51 +0000

    hgjr

---
+++ script.js
@@ -0,0 +1,5 @@
+function strucfunc() {
+
+    document.getElementById("result").innerHTML = document.getElementById("return").firstChild.nodeValue;
+    document.getElementById("result2").innerHTML = document.getElementById("result").firstChild.nodeValue;
+}
